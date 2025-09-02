193314f58d9f2246a7bf33404d88855137712d77
AYAN-SHAH <aayanshah066@gmail.com>
2025-09-02 21:44:30 +0500

    update Readme.md

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,6 +11,7 @@ export default defineConfig({
       '/uploads': 'http://localhost:4001'
     }
   }
-})
+});
+
 
 
